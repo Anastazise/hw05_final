8eadb99506e7a652270701be1820b339e47075df
Anastazise <anastazidevilfor@yandex.ru>
2022-12-16 17:31:08 +0300

    пеп8

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -2,7 +2,6 @@ import shutil
 import tempfile
 from http import HTTPStatus
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
